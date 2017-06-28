1d708acb37a7a3414a04493689c8cac2b2818cce
HippErger <berger.clayton@gmail.com>
2017-06-28 17:47:48 -0500

    setup index page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import next from 'next';
 import path from 'path';
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+// import LessonRouter from './routers/LessonRouter';
+// import SubjectRouter from './routers/SubjectRouter';
+// import TeacherProfileRouter from './routers/TeacherProfileRouter';
 
 const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({
@@ -8,17 +13,39 @@ const nextApp = next({
   dir: path.resolve(__dirname)
 });
 const handle = nextApp.getRequestHandler();
-
 const PORT = 3001;
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/aca-demo-june30');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('mongoose and db are connected'
+);
+});
 
 nextApp.prepare().then(() => {
   const app = express();
 
-  // Define all you backend handlers here...
+  // All backend handlers
+  app.use(bodyParser.json());
+  // app.use(LessonRouter);
+  // app.use(SubjectRouter);
+  // app.use(TeacherProfileRouter);
 
   // Handle everything that is not covered in above routes with next.js
   app.get('*', (request, response) => {
     return handle(request, response);
+  });
+
+  // Error handling middleware
+  // @TODO remove disable
+  // eslint-disable-next-line
+  app.use('*', (err, request, response, nexthandler) => {
+    return response.status(500).json({
+      message: err.message
+    });
   });
 
   app.listen(PORT, () => {
@@ -26,3 +53,33 @@ nextApp.prepare().then(() => {
     console.log('Server listening on port ' + PORT);
   });
 });
+
+
+// import next from 'next';
+// import path from 'path';
+// import express from 'express';
+//
+// const dev = process.env.NODE_ENV !== 'production';
+// const nextApp = next({
+//   dev,
+//   dir: path.resolve(__dirname)
+// });
+// const handle = nextApp.getRequestHandler();
+//
+// const PORT = 3001;
+//
+// nextApp.prepare().then(() => {
+//   const app = express();
+//
+//   // Define all you backend handlers here...
+//
+//   // Handle everything that is not covered in above routes with next.js
+//   app.get('*', (request, response) => {
+//     return handle(request, response);
+//   });
+//
+//   app.listen(PORT, () => {
+//     // eslint-disable-next-line
+//     console.log('Server listening on port ' + PORT);
+//   });
+// });
