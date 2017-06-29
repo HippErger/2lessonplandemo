acc1cfb6e92b166cbd6a431922f25f711d97d551
HippErger <berger.clayton@gmail.com>
2017-06-29 17:03:31 -0500

    added styles for index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,54 @@
 import React from 'react';
 import Link from 'next/link';
+import {StyleSheet, css} from 'aphrodite';
 
 function IndexPage() {
+
+  const indexStyles = StyleSheet.create({
+    root: {
+      width: 150,
+      height: 200,
+      margin: 'auto',
+      textAlign: 'center',
+      background: '#89878C',
+      borderRadius: 150,
+      paddingTop: 40,
+      ':hover': {
+        background: '#4CAF50',
+        border: '1px solid black',
+
+      }
+    },
+
+    title: {
+      margin: 'auto',
+      paddingTop: 20,
+      color: 'black',
+      fontSize: 32,
+      ':hover': {
+        color: 'white'
+      }
+    },
+
+    linkFont: {
+      margin: 'auto',
+      marginRight: 12,
+      ':hover': {
+        color: 'white'
+      }
+    }
+  });
+
   return (
-    <div>
-      <h1>
-        Sign In!
-      </h1>
+    <div className={css(indexStyles.root)}>
       <Link href="/about">
-        <a>About</a>
+        <h1 className={css(indexStyles.title)}>
+          Schmek!
+        </h1>
       </Link>
       &nbsp; &nbsp;
       <Link href={{pathname: '/profile', query: {_id: '59526f7b403e1551a0c6f73c'} }}>
-        <a>Sign-In</a>
+        <a className={css(indexStyles.linkFont)}>Sign-In</a>
       </Link>
     </div>
   );
